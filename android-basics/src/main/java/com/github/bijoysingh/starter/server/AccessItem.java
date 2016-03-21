394ed65af8ca0b1b4b5ac738d72758aa6d9dcd6c
bijoy <bijoysingh693@gmail.com>
2016-03-21 16:53:53 +0530

    Changing AccessItem to support v4 Fragments

--- android-basics/src/main/java/com/github/bijoysingh/starter/server/AccessItem.java
+++ android-basics/src/main/java/com/github/bijoysingh/starter/server/AccessItem.java
@@ -1,7 +1,7 @@
 package com.github.bijoysingh.starter.server;
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 
 import com.android.volley.Request;
 

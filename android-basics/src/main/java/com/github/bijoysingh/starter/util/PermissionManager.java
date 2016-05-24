d4a399275b63cef4fcadd446f90646502fdf2eb7
bijoy <bijoysingh693@gmail.com>
2016-05-24 09:34:15 +0530

    Added documentation for the permission Manager

--- android-basics/src/main/java/com/github/bijoysingh/starter/util/PermissionManager.java
+++ android-basics/src/main/java/com/github/bijoysingh/starter/util/PermissionManager.java
@@ -18,25 +18,52 @@ public class PermissionManager {
     private Activity activity;
     private String[] permissions;
 
+    /**
+     * Constructor for the Permission Manager
+     *
+     * @param activity the activity which will request the permissions
+     */
     public PermissionManager(Activity activity) {
         this.activity = activity;
         this.permissions = new String[]{};
     }
 
+    /**
+     * Constructor for the Permission Manager
+     *
+     * @param activity    the activity which will request the permissions
+     * @param permissions the list of permissions requested
+     */
     public PermissionManager(Activity activity, String[] permissions) {
         this.activity = activity;
         this.permissions = permissions;
     }
 
+    /**
+     * Set the requested permissions
+     *
+     * @param permissions the permissions which are needed
+     */
     public void setPermissions(String[] permissions) {
         this.permissions = permissions;
     }
 
+    /**
+     * Checks if the application has the permission
+     *
+     * @param permission the permission
+     * @return true if has permission
+     */
     public Boolean hasPermission(String permission) {
         return ContextCompat.checkSelfPermission(activity, permission)
-                == PackageManager.PERMISSION_GRANTED;
+            == PackageManager.PERMISSION_GRANTED;
     }
 
+    /**
+     * Returns a list of missing permissions
+     *
+     * @return the list permissions which the app does not have
+     */
     public String[] getMissingPermissions() {
         List<String> missingPermissions = new ArrayList<>();
         for (String permission : permissions) {
@@ -50,17 +77,31 @@ public class PermissionManager {
         return missingPermissionsArray;
     }
 
+    /**
+     * Returns if the application already has all desired permissions
+     *
+     * @return has all permissions
+     */
     public Boolean hasAllPermissions() {
         String[] missingPermissions = getMissingPermissions();
         return missingPermissions.length == 0;
     }
 
+    /**
+     * Request for permissions if the phone sdk > 23 and any permission is missing
+     *
+     * @param requestCode the request code
+     */
     public void requestPermissions(Integer requestCode) {
         if (Build.VERSION.SDK_INT >= 23 && !hasAllPermissions()) {
             activity.requestPermissions(getMissingPermissions(), requestCode);
         }
     }
 
+    /**
+     * Request for permissions if the phone sdk > 23 and any permission is
+     * missing with default request code
+     */
     public void requestPermissions() {
         requestPermissions(REQUEST_CODE_ASK_PERMISSIONS);
     }

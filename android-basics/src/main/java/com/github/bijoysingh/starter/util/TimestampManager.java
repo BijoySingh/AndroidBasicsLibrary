cf7cc711dcc0d063890c83ccb6adb2fa3648dfff
bijoy <bijoysingh693@gmail.com>
2016-05-24 09:29:35 +0530

    Added more flexible timestamp functions and added documentation

--- android-basics/src/main/java/com/github/bijoysingh/starter/util/TimestampManager.java
+++ android-basics/src/main/java/com/github/bijoysingh/starter/util/TimestampManager.java
@@ -5,14 +5,13 @@ import com.github.bijoysingh.starter.item.TimestampItem;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 
-import java.io.Serializable;
 import java.util.Calendar;
 
 /**
  * Timestamp functions
  * Created by Bijoy on 5/25/2015.
  */
-public class TimestampManager implements Serializable {
+public class TimestampManager {
     public static String SPACE = " ";
     public static String PM = "p.m.";
     public static String AM = "a.m.";
@@ -20,17 +19,28 @@ public class TimestampManager implements Serializable {
     public static String NOON = "Noon";
     public static String COLON = ":";
 
-    public static TimestampItem getTimestampItem(String timestamp) {
-        DateTime dateTime;
+    /**
+     * Get the datetime object from timestamp else returns null
+     *
+     * @param timestamp the timestamp
+     * @return DateTime object or null
+     */
+    private static DateTime getDateTimeObject(String timestamp) {
         try {
-            dateTime = new DateTime(timestamp);
+            return new DateTime(timestamp);
         } catch (Exception e) {
-            return new TimestampItem(timestamp, null, null, Calendar.getInstance());
+            return null;
         }
+    }
 
-        dateTime = dateTime.toDateTime(DateTimeZone.UTC);
-        dateTime = dateTime.plusHours(5).plusMinutes(30);
-
+    /**
+     * Creates th timestamp item from the datetime variable
+     *
+     * @param timestamp the string input timestamp
+     * @param dateTime  the date time instance
+     * @return the TimestampItem object
+     */
+    private static TimestampItem createTimestampItem(String timestamp, DateTime dateTime) {
         String MM = dateTime.monthOfYear().getAsString();
         String MMMM = dateTime.monthOfYear().getAsText();
         String dd = dateTime.dayOfMonth().getAsText();
@@ -39,13 +49,65 @@ public class TimestampManager implements Serializable {
         String yyyy = dateTime.year().getAsText();
 
         String date = dd + SPACE + MMMM + SPACE + yyyy;
-        String time = getTimeStr(hh, mm);
+        String time = get12hrFormattedTime(hh, mm);
         Calendar calender = getCalendar(yyyy, MM, dd, hh, mm);
-
         return new TimestampItem(timestamp, time, date, calender);
     }
 
-    public static String getTimeStr(String hh, String mm) {
+    /**
+     * Returns a TimestampItem object by parsing the time using DateTime library
+     *
+     * @param timestamp the timestamp object
+     * @return the TimestampItem
+     */
+    public static TimestampItem getTimestampItem(String timestamp) {
+        return getTimestampItem(timestamp, DateTimeZone.UTC, 0, 0);
+    }
+
+    /**
+     * Returns a TimestampItem object by parsing the time using DateTime library
+     *
+     * @param timestamp the timestamp object
+     * @param hour      the timezone hours to be added
+     * @param minutes   the timezone minutes to be added
+     * @return the TimestampItem
+     */
+    public static TimestampItem getTimestampItem(String timestamp,
+                                                 Integer hour, Integer minutes) {
+        return getTimestampItem(timestamp, DateTimeZone.UTC, hour, minutes);
+    }
+
+    /**
+     * Returns a TimestampItem object by parsing the time using DateTime library
+     *
+     * @param timestamp    the timestamp object
+     * @param dateTimeZone the date timezone of the input time.
+     * @param hour         the timezone hours to be added
+     * @param minutes      the timezone minutes to be added
+     * @return the TimestampItem
+     */
+    public static TimestampItem getTimestampItem(String timestamp,
+                                                 DateTimeZone dateTimeZone,
+                                                 Integer hour, Integer minutes) {
+        DateTime dateTime = getDateTimeObject(timestamp);
+        if (dateTime == null) {
+            return new TimestampItem(timestamp, null, null, Calendar.getInstance());
+        }
+
+        dateTime = dateTime.toDateTime(dateTimeZone);
+        dateTime = dateTime.plusHours(5).plusMinutes(30);
+
+        return createTimestampItem(timestamp, dateTime);
+    }
+
+    /**
+     * Returns 12 hr format for hr and minutes in 24 hrs
+     *
+     * @param hh the hours
+     * @param mm the minutes
+     * @return the 12-hr formatted string
+     */
+    public static String get12hrFormattedTime(String hh, String mm) {
         Integer hr = Integer.parseInt(hh);
         Integer min = Integer.parseInt(mm);
 
@@ -65,20 +127,30 @@ public class TimestampManager implements Serializable {
         }
     }
 
-    public static Calendar getCalendar(
-            String yyyy,
-            String MM,
-            String dd,
-            String hh,
-            String mm
+    /**
+     * Creates the calendar object from the various time values
+     *
+     * @param yyyy year
+     * @param MM   month
+     * @param dd   date
+     * @param hh   hour
+     * @param mm   minutes
+     * @return the Calendar value
+     */
+    private static Calendar getCalendar(
+        String yyyy,
+        String MM,
+        String dd,
+        String hh,
+        String mm
     ) {
         Calendar calendar = Calendar.getInstance();
         calendar.set(
-                Integer.parseInt(yyyy),
-                Integer.parseInt(MM) - 1,
-                Integer.parseInt(dd),
-                Integer.parseInt(hh),
-                Integer.parseInt(mm)
+            Integer.parseInt(yyyy),
+            Integer.parseInt(MM) - 1,
+            Integer.parseInt(dd),
+            Integer.parseInt(hh),
+            Integer.parseInt(mm)
         );
         return calendar;
     }

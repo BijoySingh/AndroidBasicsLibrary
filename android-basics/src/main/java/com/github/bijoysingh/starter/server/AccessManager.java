5ff1814e99d24a3f9203948b8127fe31b8e13cc2
bijoy <bijoysingh693@gmail.com>
2016-05-24 08:59:01 +0530

    Adding documentation for the AccessManager class

--- android-basics/src/main/java/com/github/bijoysingh/starter/server/AccessManager.java
+++ android-basics/src/main/java/com/github/bijoysingh/starter/server/AccessManager.java
@@ -22,14 +22,29 @@ import java.util.Map;
  */
 public abstract class AccessManager {
 
+    // Timeout value for the request
     public static final Integer TIMEOUT = 7500;
 
+    // The application context
     public Context context;
 
+    /**
+     * AccessManager default constructor
+     *
+     * @param context the application context
+     */
     public AccessManager(Context context) {
         this.context = context;
     }
 
+    /**
+     * Creates a volley JsonObjectRequest which will automatically call the handleSendResponse/Error
+     * functions based on situations and the various parameters of the access item object
+     *
+     * @param access the AccessItem instance
+     * @param map    the key-values to be sent
+     * @return the volley request object
+     */
     public JsonObjectRequest getJsonRequest(final AccessItem access,
                                             final Map<String, Object> map) {
         JsonObjectRequest jsonRequest = new JsonObjectRequest(
@@ -66,6 +81,13 @@ public abstract class AccessManager {
         return jsonRequest;
     }
 
+    /**
+     * Creates a volley StringRequest which will automatically call the handleGetResponse/Error
+     * functions based on situations and the various parameters of the access item object
+     *
+     * @param access the AccessItem instance
+     * @return the volley StringRequest object
+     */
     public StringRequest getStringRequest(final AccessItem access) {
         StringRequest request = new StringRequest(
             Request.Method.GET, access.url, new Response
@@ -98,24 +120,64 @@ public abstract class AccessManager {
         return request;
     }
 
+    /**
+     * Gets the string value from the server based on the request
+     *
+     * @param access the AccessItem instance
+     */
     public void get(final AccessItem access) {
         StringRequest request = getStringRequest(access);
         Volley.newRequestQueue(context).add(request);
     }
 
+    /**
+     * Sends a map object as a JSON object to the server
+     *
+     * @param access the AccessItem instance
+     * @param map    the key-values to be sent
+     */
     public void send(final AccessItem access,
                      final Map<String, Object> map) {
         JsonObjectRequest request = getJsonRequest(access, map);
         Volley.newRequestQueue(context).add(request);
     }
 
+    /**
+     * The header information which you wish to send when a request is authenticated
+     *
+     * @return the extra header key-value pairs
+     */
     public abstract Map<String, String> getAuthenticationData();
 
+    /**
+     * Handler for when a get() function gets a successful response
+     *
+     * @param access   the AccessItem instance used in the query
+     * @param response the String response from the server
+     */
     public abstract void handleGetResponse(AccessItem access, String response);
 
+    /**
+     * Handler for when a send() function gets a successful response
+     *
+     * @param access   the AccessItem instance used in the query
+     * @param response the JSON response from the server
+     */
     public abstract void handleSendResponse(AccessItem access, JSONObject response);
 
+    /**
+     * Handler for when a get() function gets an error
+     *
+     * @param access the AccessItem instance used in the query
+     * @param error  the volley error object
+     */
     public abstract void handleGetError(AccessItem access, VolleyError error);
 
+    /**
+     * Handler for when a send() function gets an error
+     *
+     * @param access the AccessItem instance used in the query
+     * @param error  the volley error object
+     */
     public abstract void handleSendError(AccessItem access, VolleyError error);
 }

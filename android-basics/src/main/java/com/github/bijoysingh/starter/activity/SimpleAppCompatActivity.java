617a91860f7f12139ad787e53b90505d88b153b4
bijoy <bijoysingh693@gmail.com>
2017-09-17 18:17:56 +0100

    Adding common view getters in Simple Activity

---
+++ android-basics/src/main/java/com/github/bijoysingh/starter/activity/SimpleAppCompatActivity.java
@@ -0,0 +1,10 @@
+package com.github.bijoysingh.starter.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bijoy on 9/17/17.
+ */
+
+public class SimpleAppCompatActivity extends AppCompatActivity {
+}
